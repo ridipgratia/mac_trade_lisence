6c192165b9490be2ec67667206c6e00c7c384fc3
ridipblog <memorytemp5@gmail.com>
2024-04-27 13:02:13 +0530

    add trade blade file

--- resources/views/trade/trade_lisence.blade.php
+++ resources/views/trade/trade_lisence.blade.php
@@ -1,1 +1,10 @@
-
+@extends('layouts.app')
+@section('title', 'Trade Lisence')
+@section('css_link')
+    <link rel="stylesheet" href="{{ asset('css/trade/style.css') }}">
+@endsection
+@section('content')
+    <div class="d-flex flex-wrap main-trade-links-div">
+        <a href=""><i class="fa-solid fa-plus"></i> Apply For New Trade License</a>
+    </div>
+@endsection
