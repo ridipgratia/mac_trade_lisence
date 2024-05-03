2cef27ad82a8a09348325b9e940b0e99ad049ad4
dipu-kr <dipum906@gmail.com>
2024-05-03 11:36:10 +0530

    updating about and facilities in home

--- resources/views/trade/trade_lisence.blade.php
+++ resources/views/trade/trade_lisence.blade.php
@@ -12,25 +12,47 @@
             </span>
 
             <h4>Information Related To Trade License</h4>
-            <span class="text-secondary fw-bold text-uppercase">
-                <h3>About</h3>
-            </span>
-            <p class="shadow" style="padding:10px;border-radius:5px;">
-                The Trade License System provides a digital interface, allowing citizens to apply for the Trade License and
-                subsequently make the payment online. Traders can apply for new licenses, renewals, amendments, and
-                supplemental
-                licenses. It streamlines and automates business licensing processes and helps a business to be set up
-                quickly.
-            </p>
-            <span class="text-secondary fw-bold text-uppercase">
-                <h3>FACILITIES</h3>
-            </span>
-            <div class="shadow">
-                <li class="list-group">Apply for new Trade License</li>
-                <li class="list-group">Apply for renewal of Trade License</li>
-                <li class="list-group">Download Trade License.</li>
-                <li class="list-group">Make Online payments</li>
-                <li class="list-group">Track application Status</li>
+            <div class="d-flex flex-wrap gap-2 justify-content-center col-12">
+                {{-- ------about start-------- --}}
+                <div class="col-md-5 col-11">
+                    <span class="text-secondary fw-bold text-uppercase">
+                        <h3>About</h3>
+                    </span>
+                    <p class="shadow"
+                        style="padding:10px;border-radius:5px; text-align: justify; min-height: 250px;height: auto;align-content: center">
+                        The Trade License System provides a digital interface, allowing citizens to apply for the Trade
+                        License and
+                        subsequently make the payment online. Traders can apply for new licenses, renewals, amendments, and
+                        supplemental
+                        licenses. It streamlines and automates business licensing processes and helps a business to be set
+                        up
+                        quickly.
+                    </p>
+                </div>
+                {{-- -------about -end ------------- --}}
+                {{-- -----facility start------ --}}
+                <div class="col-md-5 col-11">
+                    <span class="text-secondary fw-bold text-uppercase">
+                        <h3>FACILITIES</h3>
+                    </span>
+                    <div class="d-flex flex-wrap flex-column  col-12 shadow"
+                        style="padding: 10px;align-items: center; min-height: 250px;height: auto;align-content: center;justify-content: center;">
+                        <li class="list-group col-md-6 col-10" style="margin: 5px 0px; text-align: left;">1. Apply for new
+                            Trade License</li>
+                        <li class="list-group col-md-6 col-10" style="margin: 5px 0px; text-align: left;">2. Apply for
+                            renewal of
+                            Trade License</li>
+                        <li class="list-group col-md-6 col-10" style="margin: 5px 0px; text-align: left;">3. Download Trade
+                            License.
+                        </li>
+                        <li class="list-group col-md-6 col-10" style="margin: 5px 0px; text-align: left;">4. Make Online
+                            payments</li>
+                        <li class="list-group col-md-6 col-10" style="margin: 5px 0px; text-align: left;">5. Track
+                            application Status
+                        </li>
+                    </div>
+                </div>
+                {{-- -----facility end------- --}}
             </div>
         </div>
     </div>
